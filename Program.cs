726a31d35cb5e11764c2972d61f10f90a5994456
TSUNOI Taku <taku_tsunoi@mergris.com>
2017-11-17 11:38:46 +0900

    rename namespace

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace ConsoleApplication
+namespace WeekendRaytracing
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! from new namespace");
         }
     }
 }
