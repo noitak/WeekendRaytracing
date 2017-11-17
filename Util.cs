7ed7014473c1737d9fa1f1a40d46ed0082fe8448
TSUNOI Taku <taku_tsunoi@mergris.com>
2017-11-17 17:01:08 +0900

    add comment

--- Util.cs
+++ Util.cs
@@ -6,8 +6,14 @@ namespace WeekendRaytracing
     {
         public static Vec3 Color(Ray ray)
         {
+            // -1.0 < ray.Diraction < 1.0
             Vec3 unitDirection = Vec3.UnitVector(ray.Direction());
+
+            // 0.0 < t < 1.0
             double t = 0.5 * (unitDirection.y + 1.0);
+
+            // white -> blue
+            // Lerp = (1 - t) * Begin + (t * End)
             return (1.0 - t) * new Vec3(1.0, 1.0, 1.0) + (t * new Vec3(0.5, 0.7, 1.0));
         }
     }
